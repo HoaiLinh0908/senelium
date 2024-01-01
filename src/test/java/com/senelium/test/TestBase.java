71cb286a021b4012f4e799c07f6cf765cec93592
Linh Do <hoailinh9966@gmail.com>
2024-01-01 16:28:27 +0700

    add dependencies for Slf4j

--- src/test/java/com/senelium/test/TestBase.java
+++ src/test/java/com/senelium/test/TestBase.java
@@ -5,11 +5,13 @@ import com.senelium.config.DriverConfig;
 import com.senelium.factories.driver.EdgeDriverFactory;
 import com.senelium.factories.driver.manager.DriverFactoryManager;
 import com.senelium.listener.TestListener;
+import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 
+@Slf4j
 @Listeners(TestListener.class)
 public class TestBase {
 
